f85f6bd3f561816547f6262ba6786859877e773f
opengl-8080 <tomcat.port.8080+github@gmail.com>
2017-12-04 22:22:54 +0900

    サードパーティのライブラリを使えるか検証

--- src/main/groovy/sample/gradle/plugin/MyGradlePlugin.groovy
+++ src/main/groovy/sample/gradle/plugin/MyGradlePlugin.groovy
@@ -1,5 +1,6 @@
 package sample.gradle.plugin
 
+import org.apache.commons.lang3.StringUtils
 import org.gradle.api.Plugin
 import org.gradle.api.Project;;
 
@@ -9,6 +10,8 @@ class MyGradlePlugin implements Plugin<Project> {
     void apply(Project project) {
         project.task('hello') {
             doLast {
+                def result = StringUtils.join("foo", "bar")
+                println "result=$result"
                 println 'Hello My Gradle Plugin!!'
             }
         }
